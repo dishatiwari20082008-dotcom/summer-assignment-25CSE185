13ddf8d037aa1afeef58ae6b7d12f93334d6f3d9
dishatiwari20082008-dotcom <dishatiwari20082008@gmail.com>
2026-06-07 21:38:36 +0530

    Create Q18.py
    Added Q18.solution

---
+++ Day-5/Q18.py
@@ -0,0 +1,20 @@
+# Check Strong Number
+
+num = int(input("Enter a number: "))
+temp = num
+sum_factorial = 0
+
+while temp > 0:
+    digit = temp % 10
+
+    factorial = 1
+    for i in range(1, digit + 1):
+        factorial *= i
+
+    sum_factorial += factorial
+    temp //= 10
+
+if sum_factorial == num:
+    print(num, "is a Strong Number")
+else:
+    print(num, "is not a Strong Number")
