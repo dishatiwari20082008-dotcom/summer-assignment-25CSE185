bf997404647b1cb6bf1fdfaac07ec8c4644a3ebc
dishatiwari20082008-dotcom <dishatiwari20082008@gmail.com>
2026-06-07 21:39:18 +0530

    Create Q19.py
    Added Q19.solution

---
+++ Day-5/Q19.py
@@ -0,0 +1,9 @@
+# Print Factors of a Number
+
+num = int(input("Enter a number: "))
+
+print("Factors of", num, "are:")
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        print(i, end=" ")
