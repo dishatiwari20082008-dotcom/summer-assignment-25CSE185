31d99f01928a6e6bae49226904b837699fbbe6d9
dishatiwari20082008-dotcom <dishatiwari20082008@gmail.com>
2026-06-10 22:00:22 +0530

    Create Q39.py
    Added Q39.py

---
+++ Day-10/Q39.py
@@ -0,0 +1,14 @@
+# Number Pyramid
+
+rows = 5
+
+for i in range(1, rows + 1):
+    print(" " * (rows - i), end="")
+
+    for j in range(1, i + 1):
+        print(j, end="")
+
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+
+    print()
