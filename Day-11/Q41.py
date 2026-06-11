f790b1a0275e44fb308c039d0f2889dd981f12c0
dishatiwari20082008-dotcom <dishatiwari20082008@gmail.com>
2026-06-11 15:29:37 +0530

    Create Q41.py
    Added Q41.solution

---
+++ Day-11/Q41.py
@@ -0,0 +1,9 @@
+def sum_numbers(a, b):
+    return a + b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = sum_numbers(num1, num2)
+
+print("Sum =", result)
