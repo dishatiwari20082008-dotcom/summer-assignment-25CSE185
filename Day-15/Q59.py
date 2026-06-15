a46a0299b6c47517b0701eb619fc3bc44e9edfee
dishatiwari20082008-dotcom <dishatiwari20082008@gmail.com>
2026-06-15 21:14:44 +0530

    Create Q59.py
    Added Q59.solution

---
+++ Day-15/Q59.py
@@ -0,0 +1,12 @@
+# Rotate array right by one position
+
+arr = [1, 2, 3, 4, 5]
+
+last = arr[-1]
+
+for i in range(len(arr)-1, 0, -1):
+    arr[i] = arr[i-1]
+
+arr[0] = last
+
+print("Right rotated array:", arr)
