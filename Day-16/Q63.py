f85ca82b6fda8c08aada9985a7de1099dd2801c3
dishatiwari20082008-dotcom <dishatiwari20082008@gmail.com>
2026-06-16 21:46:30 +0530

    Create Q63.py
    Added Q63.solution

---
+++ Day-16/Q63.py
@@ -0,0 +1,10 @@
+# Find pair with given sum
+
+arr = [2, 7, 11, 15]
+
+target = 9
+
+for i in range(len(arr)):
+    for j in range(i+1, len(arr)):
+        if arr[i] + arr[j] == target:
+            print("Pair found:", arr[i], arr[j])
