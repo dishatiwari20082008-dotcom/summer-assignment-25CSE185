c3e590b139d50fc29364c616c704a4f70a36e63a
dishatiwari20082008-dotcom <dishatiwari20082008@gmail.com>
2026-06-20 20:51:37 +0530

    Create Q74.py
    Added Q74.solution

---
+++ Day-19/Q74.py
@@ -0,0 +1,21 @@
+r = int(input("Rows: "))
+c = int(input("Columns: "))
+
+a = []
+b = []
+
+for i in range(r):
+    a.append(list(map(int,input().split())))
+
+for i in range(r):
+    b.append(list(map(int,input().split())))
+
+result = []
+
+for i in range(r):
+    row = []
+    for j in range(c):
+        row.append(a[i][j] - b[i][j])
+    result.append(row)
+
+print(result)
