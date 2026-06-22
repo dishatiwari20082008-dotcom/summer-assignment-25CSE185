35fde95d1dd3ee22ad1bccb62f7cf2640eac9eac
dishatiwari20082008-dotcom <dishatiwari20082008@gmail.com>
2026-06-22 21:58:50 +0530

    Create Q87.py
    Added Q87.solution

---
+++ Day-22/Q87.py
@@ -0,0 +1,14 @@
+string = input("Enter a string: ")
+
+frequency = {}
+
+for char in string:
+    if char in frequency:
+        frequency[char] += 1
+    else:
+        frequency[char] = 1
+
+print("Character frequency:")
+
+for key, value in frequency.items():
+    print(key, ":", value)
