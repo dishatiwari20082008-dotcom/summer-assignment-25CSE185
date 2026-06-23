091f35d97b9a4e4e92605b11a9e15412b8560bbc
dishatiwari20082008-dotcom <dishatiwari20082008@gmail.com>
2026-06-23 21:03:02 +0530

    Create Q89.py
    Added Q89.solution

---
+++ Day-23/Q89.py
@@ -0,0 +1,8 @@
+string = input("Enter a string: ")
+
+for char in string:
+    if string.count(char) == 1:
+        print("First non-repeating character:", char)
+        break
+else:
+    print("No non-repeating character found")
