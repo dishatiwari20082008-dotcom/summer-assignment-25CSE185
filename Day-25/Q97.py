6368584d861cad166d88ba39a31d8055e2e66e21
dishatiwari20082008-dotcom <dishatiwari20082008@gmail.com>
2026-06-25 21:18:11 +0530

    Create Q97.py
    Added Q97.solution

---
+++ Day-25/Q97.py
@@ -0,0 +1,8 @@
+arr1 = list(map(int, input("Enter first sorted array: ").split()))
+arr2 = list(map(int, input("Enter second sorted array: ").split()))
+
+merged = arr1 + arr2
+
+merged.sort()
+
+print("Merged sorted array:", merged)
